70f4316d09ea36a86f770588b9d1788186abc0d1
2mol <2mol@users.noreply.github.com>
2021-01-20 12:06:00 +0100

    rename make script

--- make.sh
+++ make.sh
@@ -6,9 +6,9 @@ function build {
 	mvn clean package
 }
 
-function run-keycloak {
-	docker run --name keycloak -p 8080:8080 --env-file .env quay.io/keycloak/keycloak:12.0.1
-}
+# function run-keycloak {
+# 	docker run --name keycloak -p 8080:8080 --env-file .env quay.io/keycloak/keycloak:12.0.1
+# }
 
 function deploy-module {
 	docker cp target/example-keycloak-2fa-sms-authenticator-*.jar keycloak:/opt/jboss/keycloak/standalone/deployments/
